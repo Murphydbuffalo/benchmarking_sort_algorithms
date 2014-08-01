8d421d15e03ea63c6b3d1ccf67b4fd5f5e73c59a
Dan Murphy <murphydbuffalo@gmail.com>
2014-08-01 10:57:08 -0400

    Wrote linear and binary search methods.

---
+++ search.rb
@@ -0,0 +1,30 @@
+def linear_search(list, search_term)
+  list.each_with_index do |item, i|
+    if item == search_term
+      puts list[i]
+      return list[i]
+    end 
+  end
+  puts "#{search_term} is not in this list."
+end
+
+def binary_search(sorted_list, search_term)
+  min, max = 0, sorted_list.length - 1
+  until min == max
+    middle = (min + max) / 2
+    if sorted_list[middle] == search_term
+      puts sorted_list[middle]
+      return  sorted_list[middle] 
+    elsif sorted_list[middle] < search_term
+      min = middle + 1
+    else
+      max = middle - 1
+    end
+  end
+  puts "#{search_term} is not in this list."
+end
+
+unsorted_list = File.read('unsorted_names.txt').split(',')
+sorted_list = File.read('sorted_names.txt').split(',')
+linear_search(unsorted_list, 'Sandra')
+binary_search(sorted_list, 'Michael')
