8f8adbaa6e377e683c25b45733672f246fe1d7ad
Dan Murphy <murphydbuffalo@gmail.com>
2014-08-02 13:00:50 -0400

    Wrote a bubble sort, added it to the benchmark comparison.  Also added comparison between using while loops vs for loops for selection sort. Increased the size of the array to be sorted.

--- sorting.rb
+++ sorting.rb
@@ -1,6 +1,7 @@
 require 'benchmark'
 
-unsorted = [8, 3, 1, 9, 5, 6, 2, 4, 7, -2, 0, 8, 3, 1, 9, 5, 6, 2, 4, 7, -2, 0]
+# unsorted = [8, 3, 1, 9, 5, 6, 2, 4, 7, -2, 0, 8, 3, 1, 9, 5, 6, 2, 4, 7, -2, 0]
+unsorted = Array.new(1000) { rand(-101..100) + 1 }
 
 def selection_sort(list)
   index = 0
@@ -19,7 +20,22 @@ def selection_sort(list)
   list
 end
 
-def selection_sort_with_insert(list)
+def selection_sort_for_loops(list)
+  for index in 0..(list.length - 2)
+    min = list[index]
+    for inner_index in index..(list.length - 1)
+      if min > list[inner_index]
+        min = list[inner_index]
+        list[index], list[inner_index] = list[inner_index], list[index]
+      end
+      inner_index += 1
+    end
+    index += 1
+  end
+  list
+end
+
+def insertion_sort(list)
   index = 0
   while index < list.length - 1
     min = list[index]
@@ -37,8 +53,30 @@ def selection_sort_with_insert(list)
   list
 end
 
+def bubble_sort(list)
+  while true
+    index = 0
+    swapped = false
+    while index < (list.length - 1)
+      if list[index] > list[index + 1]
+        list[index], list[index + 1] = list[index + 1], list[index]
+        swapped = true
+      end
+      index += 1
+    end
+    return list if swapped == false
+  end
+end
+
+# p selection_sort(unsorted)
+# p selection_sort_for_loops(unsorted)
+# p insertion_sort(unsorted)
+# p bubble_sort(unsorted)
+
 Benchmark.bm do |x|
-  x.report('Swap Values:') { 10000.times do selection_sort(unsorted) end }
-  x.report('Insert Value:') { 10000.times do selection_sort_with_insert(unsorted) end }
+  x.report('Selection Sort:') { 100.times do selection_sort(unsorted) end }
+  x.report('Selection Sort With For Loops:') { 100.times do selection_sort_for_loops(unsorted) end }  
+  x.report('Insertion Sort:') { 100.times do insertion_sort(unsorted) end }
+  x.report('Bubble Sort:') { 100.times do bubble_sort(unsorted) end }
 end
 
